067309e98fd260333481148b6e7ed63f2c33021a
Lia Moua <moua0061@yahoo.com>
2021-10-08 06:45:00 -0400

    starting out with the fundamentals

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-import React from 'react';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import styled from 'styled-components';
 import './App.css';
+
+
+
+//styled componenents goes here!!!!!!
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +15,24 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [starWarsData, setStarsData] = useState([]);
+  const [starWarsInfo, setStarWarsInfo] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+      .then(resp => {
+        console.log(resp.data)
+          
+          return setStarsData(resp.data)
+      })
+      .catch ( err => {
+        console.log('your shit ain\'t working!!');
+      })
+  })
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Characters</h1>
     </div>
   );
 }
